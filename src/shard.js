bb05026fa27b727ee3a8f108edba5e40b5a0610b
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-05-31 22:46:32 +0200

    added api for bot stats

--- src/shard.js
+++ src/shard.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 const MessageHandler = require("./utils/messageHandler");
 const Logger = require("./utils/logger");
+const discordbots = require("./utils/discordbots");
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
     Logger.info(`Logged in as ${client.user.tag}!`);
+    discordbots(client);
 });
 
 client.on('message', msg => {
---
+++ src/utils/discordbots.js
@@ -0,0 +1,23 @@
+const Request = require("request");
+const util = require("util");
+const { discordbotsapi, discordbotsapi2 } = require("../../config.json");
+
+const post = util.promisify(Request.post);
+
+module.exports = (client) => {
+    post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`, {
+        headers : {
+            "Authorization" : discordbotsapi
+        },
+        json: true,
+        body: {"server_count": client.guilds.size}
+    }).catch((err) => Logger.error(`Shard[?]: discordbotapi error?`));
+
+    post(`https://discordbots.org/api/bots/${client.user.id}/stats`, {
+        headers : {
+            "Authorization" : discordbotsapi2
+        },
+        json: true,
+        body: {"server_count": client.guilds.size}
+    }).catch((err) => Logger.error(`Shard[?]: discordbotapi error?`));
+};
